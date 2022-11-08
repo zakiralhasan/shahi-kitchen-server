8c39debd181b01f4232226907f951da24cf730a0
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-08 22:52:04 +0600

    mongodb user and password created

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+//used for .env file active or use.
+require("dotenv").config();
+
+const services = require("./services.json");
 
 // midle ware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.krkb3gw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+//called the main mongoDB working function
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("server is running");
