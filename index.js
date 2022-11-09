687a47272056ca41b5aa38920f4d3472d47323f0
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-09 23:24:03 +0600

    singel review delete from reviews collection on mongodb

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       const result = await reviewsCollection.insertOne(review);
       res.send(result);
     });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
